c01c008eb0dfa22430f81670b3b95219d8819801
Luis Guisado <lguisadom@gmail.com>
2020-07-07 16:24:45 -0500

    Inicio

---
+++ src/main/java/com/lagm/PokeApiCientApplication.java
@@ -0,0 +1,13 @@
+package com.lagm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiCientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiCientApplication.class, args);
+	}
+
+}
