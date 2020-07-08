f151bd58fdc059e63c99ef38deb2dab50d66f29d
Luis Guisado <lguisadom@gmail.com>
2020-07-08 02:06:03 -0500

    Correccion paginacion

--- src/main/java/com/lagm/dto/PokemonDetailResponse.java
+++ src/main/java/com/lagm/dto/PokemonDetailResponse.java
@@ -11,4 +11,20 @@ public class PokemonDetailResponse {
 	private Color color;
 	private List<EggGroup> egg_groups;
 	private EvolutionChain evolution_chain;
+	private Pokemon evolves_from_species;
+	private List<FlavorTextEntrie> flavor_text_entries;
+	private int gender_rate;
+	private List<Genera> genera;
+	private Generation generation;
+	private GrowthRate growth_rate;
+	private Boolean has_gender_differences;
+	private int hatch_counter;
+	private int id;
+	private Boolean is_baby;
+	private String name;
+	private List<NamePokemon> names;
+	private int order;
+	private List<PokedexNumber> pokedex_numbers;
+	private Shape shape;
+	private List<Varietie> varieties;
 }
